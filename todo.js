60fbd6ddfad8e2b4240bdfc181f63fb29794e0cd
Iswanul Umam <umam@sepulsa.com>
2018-04-11 10:29:38 +0700

    first initial

---
+++ todo.js
@@ -0,0 +1,20 @@
+/**
+ * @author: Iswanul Umam - Red Fox
+ *
+ */
+
+/*
+
+available command:
+$ node todo.js
+$ node todo.js help
+$ node todo.js list
+$ node todo.js add <task_content>
+$ node todo.js findById <id>
+$ node todo.js delete <id>
+$ node todo.js complete <id>
+$ node tood.js uncomplete <id>
+
+*/
+
+console.log(`#################### TODO LIST ####################`);

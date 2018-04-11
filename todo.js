4baae0c2119acb3f2d6b386a189d8213fcaaf0b3
restuu <restu_u@outlook.com>
2018-04-11 14:12:38 +0700

    first commit

--- todo.js
+++ todo.js
@@ -1,1 +1,16 @@
+var command = process.argv
 
+const Controller = require('./controller');
+
+
+// console.log(argv);
+if (command[2] === undefined || command[2] === 'help') {
+  Controller.fromHelpNode()
+} else if (command[2] === 'list') {
+  Controller.getListFromJson()
+} else if (command[2] === 'add') {
+  let addition = command.slice(3)
+  Controller.addListToJson(addition)
+} else if (command[2] === 'findByID') {
+  Controller.findByID(command[3])
+}

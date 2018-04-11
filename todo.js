22216bc04bd466f476d4a3c0c39f75ab3c6503c1
Iswanul Umam <umam@sepulsa.com>
2018-04-11 10:42:39 +0700

    add all command list

--- todo.js
+++ todo.js
@@ -18,3 +18,43 @@ $ node tood.js uncomplete <id>
 */
 
 console.log(`#################### TODO LIST ####################`);
+
+let command = process.argv.slice(2);
+
+switch(command[0]) {
+  case 'help' : {
+    console.log('command help');
+    break;
+  }
+  case 'list': {
+    console.log('command list');
+    break;
+  }
+  case 'add': {
+    console.log('command add');
+    break;
+  }
+  case 'findById': {
+    console.log('command find by id');
+    break;
+  }
+  case 'delete': {
+    console.log('command delete');
+
+    break;
+  }
+  case 'complete': {
+    console.log('command complete');
+
+    break;
+  }
+  case 'uncomplete': {
+    console.log('command uncomplete');
+
+    break;
+  }
+  default: {
+    console.log('command help');
+    break;
+  }
+} 

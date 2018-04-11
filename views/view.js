19b137967f3565d8abec4d479cb8fd54470ec3ca
A N Afifudin <33721254+davidjoshua87@users.noreply.github.com>
2018-04-12 02:18:43 +0700

    Edit view.js list help edit

--- views/view.js
+++ views/view.js
@@ -13,7 +13,7 @@ class Views {
       '$ node todo.js help                                      # Menampilkan command apa saja yang tersedia',
       '$ node todo.js list                                      # Melihat daftar TODO',
       '$ node todo.js add <task_content>                        # Menambahkan TODO ke dalam list',
-      '$ node todo.js findBy <task_id>                          # Melihat detail TODO sesuai task_id nya',
+      '$ node todo.js findById <task_id>                          # Melihat detail TODO sesuai task_id nya',
       '$ node todo.js delete <task_id>                          # Menghapus TODO sesuai task_id nya',
       '$ node todo.js complete <task_id>                        # Menandai status TODO selesai',
       '$ node todo.js uncomplete <task_id>                      # Menandai status TODO belum selesai',
